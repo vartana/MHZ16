5f04364dd003ed22ad1fc1792f3333a036401043
Vartan Arabyan <vartana@gmail.com>
2016-02-22 15:11:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/ttyAMA0", {
-  baudrate: 9600,
-  timeout: 1
+  baudrate: 9600
 });
 
-var cmd_zero_sensor = "\xff\x87\x87\x00\x00\x00\x00\x00\xf2";
-var cmd_span_sensor = "\xff\x87\x87\x00\x00\x00\x00\x00\xf2";
-var cmd_get_sensor = "\xff\x01\x86\x00\x00\x00\x00\x00\x79";
+var cmdReq = [0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79];
+var cmdRes = [0xff, 0x86, 0x02, 0x60, 0x47, 0x00, 0x00, 0x00, 0xd1];
 
 serialPort.on("open", function () {
   console.log('open');
@@ -15,7 +13,7 @@ serialPort.on("open", function () {
   });
   
   
-  serialPort.write(cmd_get_sensor, function(err, results) {
+  serialPort.write(cmdRes, function(err, results) {
     if(err){
       console.log('err ' + err);
     }
@@ -23,3 +21,8 @@ serialPort.on("open", function () {
     console.log('results ' + results);
   });
 });
+
+
+// var cmd_zero_sensor = "\xff\x87\x87\x00\x00\x00\x00\x00\xf2";
+// var cmd_span_sensor = "\xff\x87\x87\x00\x00\x00\x00\x00\xf2";
+// var cmd_get_sensor = "\xff\x01\x86\x00\x00\x00\x00\x00\x79";
