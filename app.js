919b3a03d1ac316fb5f452ffea3d13dcc38baede
Vartan Arabyan <vartana@gmail.com>
2016-02-22 13:11:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-var i2c = require('i2c-bus');
+var i2c = require('i2c-bus'),
+  bus = i2c.openSync(1);
 
 var add = 0x4d;
 var cmd = [0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79];
 
-i2c.open(1, function(bus){
-  
-  bus.i2cWrite(add, cmd.length, cmd, console.log);
-})
+bus.i2cWrite(add, cmd.length, cmd, console.log);
