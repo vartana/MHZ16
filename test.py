efda123b110be58cdc4d402837730e058b23baa5
Vartan Arabyan <vartana@gmail.com>
2016-02-22 15:58:06 -0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=1)
-
-while True:
-    port.write("\r\nSay something:")
-    rcv = port.read(10)
-    port.write("\r\nYou sent:" + repr(rcv))
