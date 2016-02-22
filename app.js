b24a1d633216469f277eff9c0f8a5bcc2059f5aa
Vartan Arabyan <vartana@gmail.com>
2016-02-22 13:58:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ var cmdRes = [0xff, 0x86, 0x02, 0x60, 0x47, 0x00, 0x00, 0x00, 0xd1];
 
 console.log("Send");
 cmdReq.forEach(function(entry) {
-    var x = bus.sendByte(addr, entry, function(val){
-       console.log(val);
+    var x = bus.sendByte(addr, entry, function(err){
+       console.log(err);
     })
 });
 
 console.log("Receive");
 cmdRes.forEach(function(entry) {
-    var x = bus.readByteSync(addr)
-    console.log(x);
+    var x = bus.readByte(addr, 0x86, function(err, byte){
+      
+      console.log(err, byte);
+    })
+    
 });
 
