377d6df3609b12681969f4bda526ddaeac47b42e
Vartan Arabyan <vartana@gmail.com>
2016-02-22 13:27:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ var i2c = require('i2c-bus'),
   bus = i2c.openSync(1);
 
 var addr = 0x4d;
-var cmd = [0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79];
+var cmdReq = [0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79];
+var cmdRes = [0xff, 0x86, 0x02, 0x60, 0x47, 0x00, 0x00, 0x00, 0xd1];
 
-//bus.sendByteSync(addr, byte)
-
-cmd.forEach(function(entry) {
+console.log("Send");
+cmdReq.forEach(function(entry) {
     bus.sendByteSync(addr, entry)
     console.log(entry);
 });
+
+console.log("Receive");
+cmdReq.forEach(function(entry) {
+    bus.receiveByteSync(addr)
+    console.log(entry);
+});
+
