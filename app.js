d5ee43fde7d118639516639953abd86e1af3d5d7
Vartan Arabyan <vartana@gmail.com>
2016-02-22 14:12:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ var cmd_span_sensor = "\xff\x87\x87\x00\x00\x00\x00\x00\xf2";
 var cmd_get_sensor = "\xff\x01\x86\x00\x00\x00\x00\x00\x79";
 
 
-cmdReq.forEach(function(entry) {
-    console.log("Send");
-    var x = bus.readWordSync(addr, cmd_get_sensor);
-    cosnole.log(x);
-});
+  console.log("Send");
+  var x = bus.readWordSync(addr, cmd_get_sensor);
+  console.log(x);
 
 console.log("Receive");
