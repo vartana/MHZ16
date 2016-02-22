d379dbc97d84dda8048fd5364d9d637c5f3dc8d1
Vartan Arabyan <vartana@gmail.com>
2016-02-22 15:16:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ var cmdReq = new Buffer([0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]);
 var cmdRes = new Buffer([0xff, 0x86, 0x02, 0x60, 0x47, 0x00, 0x00, 0x00, 0xd1]);
 
 serialPort.on("open", function () {
-  console.log('open');
   serialPort.on('data', function(data) {
-    console.log('data received: ' + data);
+    console.log('Data: ' + data);
   });
   
+  serialPort.on('error', function(data) {
+    console.log('Error: ' + data);
+  });
   
   serialPort.write(cmdReq, function(err, results) {
     if(err){
@@ -21,8 +23,3 @@ serialPort.on("open", function () {
     console.log('results ' + results);
   });
 });
-
-
-// var cmd_zero_sensor = "\xff\x87\x87\x00\x00\x00\x00\x00\xf2";
-// var cmd_span_sensor = "\xff\x87\x87\x00\x00\x00\x00\x00\xf2";
-// var cmd_get_sensor = "\xff\x01\x86\x00\x00\x00\x00\x00\x79";
