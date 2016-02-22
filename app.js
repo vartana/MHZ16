a3e2b891ae8a0562c9a95b5e3ee28f4b43b0e94f
Vartan Arabyan <vartana@gmail.com>
2016-02-22 15:39:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/ttyAMA0", {
-  baudrate: 9600,
-  parser: Serialport.parsers.readline('\r')
+  baudrate: 9600
 });
 
-var cmdReq = new Buffer([0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]);
+var cmdReq = [0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79];
 var cmdRes = new Buffer([0xff, 0x86, 0x02, 0x60, 0x47, 0x00, 0x00, 0x00, 0xd1]);
 
 serialPort.on("open", function () {
@@ -16,19 +15,22 @@ serialPort.on("open", function () {
     console.log('Error: ' + data);
   });
   
-  serialPort.write(cmdReq, function(err, results) {
-    if(err){
-      console.log('err ' + err);
-    }
-    
-    console.log('results ' + results);
-  });
+  cmdReq.forEach(function(ent){
+    serialPort.write(ent, function(err, results) {
+      if(err){
+        console.log('err ' + err);
+      }
+      
+      console.log('results ' + results);
+    });
+  })
   
-  serialPort.write(cmdRes, function(err, results) {
-    if(err){
-      console.log('err ' + err);
-    }
+  
+  // serialPort.write(cmdReq, function(err, results) {
+  //   if(err){
+  //     console.log('err ' + err);
+  //   }
     
-    console.log('results ' + results);
-  });
+  //   console.log('results ' + results);
+  // });
 });
