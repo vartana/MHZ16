bebf6fce518ea048c8235b58eec6eaa355050d14
Vartan Arabyan <vartana@gmail.com>
2016-02-22 12:10:27 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+var i2c = require('i2c-bus'),
+  bus = i2c.openSync(1);
+
+var add = 0x4d;
+var cmd = new Buffer([0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]);
+
+bus.i2cWrite(add, cmd.length, cmd, console.log);
