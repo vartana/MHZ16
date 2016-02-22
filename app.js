606b17e18ddceb0df3eee5ab0a215f8b6728c79a
Vartan Arabyan <vartana@gmail.com>
2016-02-22 16:28:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var serialPort = new SerialPort("/dev/ttyAMA0", {
   baudrate: 9600
 });
 
-var cmdReq = [0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79];
+var cmdReq = new Buffer([0xff, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]);
 var cmdRes = new Buffer([0xff, 0x86, 0x02, 0x60, 0x47, 0x00, 0x00, 0x00, 0xd1]);
 
 serialPort.on("open", function () {
@@ -15,22 +15,11 @@ serialPort.on("open", function () {
     console.log('Error: ' + data);
   });
   
-  cmdReq.forEach(function(ent){
-    serialPort.write(ent, function(err, results) {
-      if(err){
-        console.log('err ' + err);
-      }
-      
-      console.log('results ' + results);
-    });
-  })
-  
-  
-  // serialPort.write(cmdReq, function(err, results) {
-  //   if(err){
-  //     console.log('err ' + err);
-  //   }
+  serialPort.write(cmdReq, function(err, results) {
+    if(err){
+      console.log('err ' + err);
+    }
     
-  //   console.log('results ' + results);
-  // });
+    console.log('results ' + results);
+  });
 });
